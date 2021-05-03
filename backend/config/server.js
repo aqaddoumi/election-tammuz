c9a670e4addc7241804247058511242f9505dfbe
aqaddoumi <qaddoumi.adel@gmail.com>
2021-05-04 00:46:14 +0300

    multipart file upload

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'http://api.election-tammuz.org',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '01d000485b2d9a265eb8ad4b9123cc2d'),
